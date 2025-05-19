2c61e9a8347e5abf65f6f79b737fbc7003e156f9
Saida Shaik <saidashaik6293@gmail.com>
2025-05-19 07:36:01 +0530

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var hello = "Hello, World!";
+        Console.WriteLine(hello);
+    }
+}
---
+++ Variables -Data Types/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        int age = 32;
+        string name = "Saida";
+        byte[] data = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        decimal length = 1.2M;
+        double 
+    }
+}
