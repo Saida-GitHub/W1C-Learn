c4bd0fdb1dd4ff7fba332f3e3e5bca120479b9e1
Saida Shaik <saidashaik6293@gmail.com>
2025-05-19 08:13:54 +0530

    Day2- Data Types - Completed

--- Variables -Data Types/Program.cs
+++ Variables -Data Types/Program.cs
@@ -2,11 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        // Console.WriteLine("Hello, World!");
+        //Data Types ::
         int age = 32;
         string name = "Saida";
         byte[] data = { 1, 2, 3, 4, 5, 6, 7, 8 };
         decimal length = 1.2M;
-        double 
+        double height = 5.5;
+        float rainDancetty = 2.3F;
+
+        //Operators :: +,-,/,*,<,>,<=,>=
+        // Incremant - ++
+        // Decrement - --
+
+        age = age + 2;
+        Console.WriteLine(age;
+
+        name = name + "Shaik";
+        Console.WriteLine(name);
+
+        length = length + 5;
+        Console.WriteLine(length);
+
+        age = age * 4;
+        Console.WriteLine(age);
+
+        age = age / 2;
+        Console.WriteLine(age);
     }
 }
